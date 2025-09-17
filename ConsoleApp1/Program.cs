a9ad841b5d6b7ec7a80758f1bac2cd0ef37de323
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-09-17 20:14:08 -0300

    Modifique del método Main para crear y mostrar información de una ColeccionMultiple, incluyendo la implementacion de un nuevo método informar para esta coleccion

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*Ejercicio 9
+Modifique el módulo main para crear una ColeccionMultiple e informe con esta colección.
+main
+pila = new Pila()
+cola = new Cola()
+multiple = new ColeccionMultiple(pila, cola)
+llenar(pila)
+llenar(cola)
+informar(pila)
+informar(cola)
+informar(multiple)*/
+
+using System;
 using System.Collections.Generic;
 
 namespace ConsoleApp1
@@ -10,13 +22,18 @@ namespace ConsoleApp1
             //E7
             Pila pila = new Pila();
             Cola cola = new Cola();
+            ColeccionMultiple multiple = new ColeccionMultiple(pila, cola);
             llenar(pila);
             llenar(cola);
             Console.WriteLine("PILA:");
             informar(pila);
             Console.WriteLine("COLA:");
             informar(cola);
+            Console.WriteLine("Coleccion multiple:");
+            informar(multiple);
         }
+        //Zona de metodos
+
         //E5
         public static void llenar(Coleccionable col)
         {
@@ -39,6 +56,24 @@ namespace ConsoleApp1
             {
                 Comparable numero = new Numero(int.Parse(Console.ReadLine()));
                 if (c.contiene(numero)) { Console.WriteLine("El elemento leido esta en la coleccion"); }
+                else { Console.WriteLine("El elemento no esta en la coleeccion"); }
+            }
+            catch (Exception) { Console.WriteLine("Ingreso invalido"); }
+
+        }
+        //E9
+        //Agrego el metodo informar que recibe una c. multiple y muestra la info
+        public static void informar(ColeccionMultiple cm)
+        {
+            Console.WriteLine("Cant de elementos: {0}", cm.cuantos());
+            Console.WriteLine("Min {0}", cm.minimo());
+            Console.WriteLine("Max {0}", cm.maximo());
+            // asumo que informar debe ser similar al anterior por lo tanto agrego la busquedad del elemento
+            Console.WriteLine("Ingrese un numero a buscar: ");
+            try
+            {
+                Comparable numero = new Numero(int.Parse(Console.ReadLine())); // creo un numero comparaable
+                if (cm.contiene(numero)) { Console.WriteLine("El elemento leido esta en la coleccion"); }
                 else { Console.WriteLine("El elemento no esta en la coleeccion"); }
             }
             catch (Exception) { Console.WriteLine("Ingreso invalido"); }
