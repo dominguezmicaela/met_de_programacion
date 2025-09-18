3a23f8c7749b286289e93b9205a8c5add5426516
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-09-18 18:48:33 -0300

    cambie metodos acceso

--- ConsoleApp1/Comparable.cs
+++ ConsoleApp1/Comparable.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 namespace ConsoleApp1
 {
     public interface Comparable
     {   //Asumo que el parametro es del mismo tipo que la clase que implementa la interfaz
-        bool sosIgual(Comparable c);
-        bool sosMenor(Comparable c);
-        bool sosMayor(Comparable c);
+        public bool sosIgual(Comparable c);
+        public bool sosMenor(Comparable c);
+        public bool sosMayor(Comparable c);
     }
 }
