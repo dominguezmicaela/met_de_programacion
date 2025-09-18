fc129ac7780bc5a93acf266f1578771f095b7996
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-09-18 20:58:25 -0300

    Arregle las subclase porNombre,PorDNI,PorLegajo

--- ConsoleApp1/PorLegajo.cs
+++ ConsoleApp1/PorLegajo.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     public class PorLegajo : EstrategiaDeComparacion
     {//arreglar
         public bool sosIgual(Alumno alumno1, Alumno alumno2) { return alumno1.getLegajo() == alumno2.getLegajo(); }//comparo por legajo
-        //el metodo .compare() devuelve <0 si el primero es menor,0 iguales,>0 si el primero es mayor y agregue to lower para que haga todo en minuscula
+
         public bool sosMayor(Alumno alumno1, Alumno alumno2) { return alumno1.getLegajo() > alumno2.getLegajo(); }
         public bool sosMenor(Alumno alumno1,Alumno alumno2){ return alumno1.getLegajo() < alumno2.getLegajo(); }
     }
