1bdd4079a72aa376e8a113fd40a63a5cf5b94d0b
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-09-24 20:47:57 -0300

    Agregue el uml del patron factory method

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fa94f1aaaddf334c8ba264583498c651885b98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32a53aa070f3e209d83ff3ce5e09c2119ea710f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
