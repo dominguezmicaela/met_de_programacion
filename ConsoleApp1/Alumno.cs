0cebed699ed055e443a69eb479802b892b26b22d
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-09-27 21:08:01 -0300

    implemente los metodos prestarAtencion  y distraerse en la clase Alumno

--- ConsoleApp1/Alumno.cs
+++ ConsoleApp1/Alumno.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 namespace ConsoleApp1
 {
     public class Alumno : Persona, Comparable
@@ -25,8 +26,19 @@ namespace ConsoleApp1
         public override bool sosMayor(Comparable c) { return estrategia.sosMayor(this, (Alumno)c); }
         //implemento un metodo  para que se pueda cambiar el tipo de estrateg¡ia... por nombre o por dni etc
         public void setEstrategia(EstrategiaDeComparacion nuevaEstrategia) { this.estrategia = nuevaEstrategia; }
-       override public string ToString() {
-      return string.Format("[Alumno: Nombre={0}, DNI={1}, Legajo={2}, Promedio={3}]",nombre, dni, legajo, promedio);}
+        //implemento dos metodos P3E11
+        public void prestarAtencion(){ Console.WriteLine("Prestando atencion...."); }
+        public void distraerse(){
+            //elegir aleatoriamente una frase de un arrglo
+            Random rand = new Random();
+            string[] distracciones = ["Mirando celular","Dibujando en el margen de la carpeta", "tirando aviones de papel"];
+            int pos = rand.Next(distracciones.Length);
+            Console.WriteLine(distracciones[pos]);
+        }
+       override public string ToString()
+        {
+            return string.Format("[Alumno: Nombre={0}, DNI={1}, Legajo={2}, Promedio={3}]", nombre, dni, legajo, promedio);
+        }
 
 
      }
