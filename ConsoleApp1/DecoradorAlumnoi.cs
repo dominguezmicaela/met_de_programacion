04dafd97cb2de6d34caab7d25ba6cd0adf72d1a1
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-10-15 20:11:48 -0300

    Orden Decorador Alumno

--- ConsoleApp1/DecoradorAlumnoi.cs
+++ ConsoleApp1/DecoradorAlumnoi.cs
@@ -4,25 +4,23 @@ namespace ConsoleApp1
 {
     public abstract class DecoradorAlumno : IAlumno
     {
-        //a
         protected IAlumno adicional;
         //constructor
-        public DecoradorAlumno(IAlumno adicional)
-        {
-            this.adicional = adicional;
-        }
+        public DecoradorAlumno(IAlumno adicional) { this.adicional = adicional; }
+        //propiedades
         public string Nombre { get { return adicional.Nombre; } set { adicional.Nombre = value; } }
         public string Apellido { get { return adicional.Apellido; } set { adicional.Apellido = value; } }
         public int Dni { get { return adicional.Dni; } set { adicional.Dni = value; } }
         public int Legajo { get { return adicional.Legajo; } set { adicional.Legajo = value; } }
         public float Promedio { get { return adicional.Promedio; } set { adicional.Promedio = value; } }
         public float Calificacion { get { return adicional.Calificacion; } set { adicional.Calificacion = value; } }
-        //met
+        //metodos
         public virtual string mostrarCalificacion(){ return adicional.mostrarCalificacion();}
         //Correccion casteos
         public bool sosIgual(Comparable c) { return adicional.sosIgual((DecoradorAlumno)c); }
         public bool sosMenor(Comparable c) { return adicional.sosMenor((DecoradorAlumno)c); }
         public bool sosMayor(Comparable c) { return adicional.sosMayor((DecoradorAlumno)c); }
+        //agregar el metodo responder pregunta
         public int responderPregunta(int pregunta){ return this.adicional.responderPregunta(pregunta); }
          public override string ToString(){return  adicional.ToString();}
         
