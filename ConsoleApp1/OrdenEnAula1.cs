f9aa24b2fa5f298f7758ded412e47014282ad042
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-10-24 11:34:38 -0300

    Se agrego la interface OrdenEnAula1

---
+++ ConsoleApp1/OrdenEnAula1.cs
@@ -0,0 +1,9 @@
+using System;
+namespace ConsoleApp1
+{
+    public interface OrdenEnAula1
+    {
+        //metodo
+        public void ejecutar();
+    }
+}
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd2be589200ba48f88fa5907369fea50e0eaf71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6dbdf33950a45ff2bc031f414e4e7bc3b8d6d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
