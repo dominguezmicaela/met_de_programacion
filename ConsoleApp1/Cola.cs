0693140d78ada08d25854cdde7d93f84ebac9a25
Micaela Dominguez <dominguezmicaela418@gmail.com>
2025-10-24 14:08:19 -0300

    Cola implementa interface Ordenable, modificacion en encolar.

--- ConsoleApp1/Cola.cs
+++ ConsoleApp1/Cola.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 namespace ConsoleApp1
 {
-    public class Cola : Coleccionable,Iterable
+    public class Cola : Coleccionable,Iterable,Ordenable
     {
         //atributos
         private List<Comparable> elementos;
+        private OrdenEnAula1 ordenInicio, ordenAulaLlena;
+        private OrdenEnAula2 ordenLlegaAlumno;
         //constructor
         public Cola() { this.elementos = new List<Comparable>(); }
         //propiedades
@@ -26,7 +28,14 @@ namespace ConsoleApp1
             }
             else { throw new Exception("Cola vacia"); }
         }
-        public void encolar(Comparable c) { this.elementos.Add(c); }// encolar recibe c y lo agrega
+        public void encolar(Comparable c)
+        {
+
+            this.elementos.Add(c);
+            if (this.cuantos() == 1 && this.ordenInicio != null) { this.ordenInicio.ejecutar(); }
+            if (this.ordenLlegaAlumno != null) { this.ordenLlegaAlumno.ejecutar(c); }
+            if(this.cuantos()==40 && this.ordenAulaLlena!= null){ this.ordenAulaLlena.ejecutar(); }
+        }// encolar recibe c y lo agrega
         public void desencolar()
         {
             if (cuantos() > 0) { this.elementos.RemoveAt(0); }
@@ -59,7 +68,10 @@ namespace ConsoleApp1
             else { throw new Exception("Cola vacia"); }
         }
         public bool estaVacia() { return this.cuantos() == 0; }
-        public Iterador crearIterador(){return new IteradorCola<Comparable>(this);}
+        public Iterador crearIterador() { return new IteradorCola<Comparable>(this); }
+        public void setOrdenInicio(OrdenEnAula1 orden){ this.ordenInicio = orden; }
+        public void setOrdenLlegaAlumno(OrdenEnAula2 orden){ this.ordenLlegaAlumno = orden; }
+        public void setOrdenAulaLlena(OrdenEnAula1 orden){ this.ordenAulaLlena = orden; }
         
     }
 }
